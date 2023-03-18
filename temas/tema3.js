d0d46717e9ca37055f1a27d309ac98dffa32b0f4
ESTEFANIAAVILA89 <105684764+ESTEFANIAAVILA89@users.noreply.github.com>
2023-03-17 23:34:42 -0300

    ejemplos for y while

--- temas/tema3.js
+++ temas/tema3.js
@@ -51,7 +51,6 @@
 // let numero = prompt("ingrese telefono");
 // do {
 
-
 //   if (numero.length === 10) {
 //     alert("numero correcto");
 //   } else {
@@ -77,14 +76,64 @@
 
 // (AHORA CON FOR) Escribe un algoritmo que muestre los numeros del 1 al 10 de mayor a menor.
 
-// for (let i = 10; i > 0; i--) {
+// for (let i = 10; i >= 1; i--) {
 //   console.log(i);
 // }
 
 // (AHORA CON FOR) Escribe un algoritmo que muestre si cada uno de los numeros del 1 al 10 son múltiplos de 2.
 
+// for (let index = 1; index <=10 ;index++) {
+
+//   if (index % 2 ===0) {
+//     console.log(index)
+//   }
+// }
+
 // Escribe un algoritmo que muestre solo los numeros pares del 1 al 10
+// for (let i = 1; i <=10; i++) {
 
-// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un juego de 10 preguntas y respuestas donde cada respuesta correcta sume un punto. Al final del juego debe mostrar cuantos puntos sumo el jugador.
+//   i++
+//   console.log(i
+//     )
+// }
 
-// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un algoritmo que pida dos numeros y la operacion a realizar (+, -, *, /) y muestre el resultado. Validar que los datos ingresados sean correctos y volver a pedirlos si es necesario.
+// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un juego de 10 preguntas
+// y respuestas donde cada respuesta correcta sume un punto. Al final del juego debe mostrar
+//cuantos puntos sumo el jugador.
+// let pregunta1 ="en que fecha salio campeon argentina?";
+//  let pregunta2 ="cuando son las elecciones en tucuman?";
+// let pregunta3 ="en que fecha se juega el partido amistoso argentina-panama?";
+
+// let respuesta1 = "18 de diciembre del 2023";
+//  let respuesta2="22 de mayo del 2023";
+// let respuesta3="23 de marzo del 2023";
+
+// if (
+//   pregunta1 === respuesta1 &&
+//   pregunta2 === respuesta2 &&
+//   pregunta3 === respuesta3
+// ) {
+//   console.log("tienes 3 puntos");
+// } else if (
+//   pregunta1 === respuesta1 &&
+//   pregunta2 === respuesta2 &&
+//   pregunta3 !== respuesta3
+// ) {
+//   console.log("tiene dos puntos");
+// } else if (
+//   pregunta1 === respuesta1 &&
+//   pregunta2 !== respuesta2 &&
+//   pregunta3 === respuesta3
+// ) {
+//   console.log("tiene dos puntos");
+// } else if (
+//   pregunta1 !== respuesta1 &&
+//   pregunta2 === respuesta2 &&
+//   pregunta3 === respuesta3
+// ) {
+//   console.log("tiene dos puntos");
+// }
+
+// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un algoritmo que pida dos numeros y
+//la operacion a realizar (+, -, *, /) y muestre el resultado. Validar que los datos ingresados
+//sean correctos y volver a pedirlos si es necesario.
